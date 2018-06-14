4381927b32bd53152a4c144876aa18f0b3dad32a
Thanga Ganapathy <ganapathy888@gmail.com>
2018-06-14 13:32:32 +0530

    Remove external port finder

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,18 +2,10 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const portfinder = require('portfinder');
-
-// Returns available port number
-function getPortNo() {
-  portfinder.basePort = 8080;
-  portfinder.getPort((err, port) => port);
-}
 
 module.exports = merge(common, {
   devtool: 'cheap-module-source-map',
   devServer: {
-    port: getPortNo(),
     contentBase: './public',
     historyApiFallback: true,
   },
