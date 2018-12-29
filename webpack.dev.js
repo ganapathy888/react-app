515e6b4939663c308b3fc10f2379ab47f6508766
Thanga Ganapathy <ganapathy888@gmail.com>
2018-12-29 16:40:44 +0530

    Change devtool to eval

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,10 @@
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  devtool: 'cheap-module-source-map',
+  devtool: 'eval',
   devServer: {
     contentBase: './public',
     historyApiFallback: true,
